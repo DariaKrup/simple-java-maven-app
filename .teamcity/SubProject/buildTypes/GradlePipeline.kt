bc72c593c1384abd3669554af9f3371ad38dec6f
Daria Krupkina <daria.krupkina@jetbrains.com>
2026-01-22 11:58:51 +0100

    Remove schedule trigger, add context parameter

--- .teamcity/SubProject/buildTypes/GradlePipeline.kt
+++ .teamcity/SubProject/buildTypes/GradlePipeline.kt
@@ -2,30 +2,38 @@ package SubProject.buildTypes
 
 import SubProject.vcsRoots.GradleConnectionRoot
 import SubProject.vcsRoots.MavenUnbalancedRoot
+import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.pipelines.Pipeline
 import jetbrains.buildServer.configs.kotlin.triggers.ScheduleTrigger
 import jetbrains.buildServer.configs.kotlin.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
+
+
 object GradlePipeline : Pipeline({
     name = "Gradle Pipeline"
 
+    val parameterName = DslContext.getParameter("parameter_name")
     repositories {
         main(GradleConnectionRoot)
         additional(MavenUnbalancedRoot)
     }
 
+    params {
+        text("parameter", parameterName)
+    }
+
     triggers {
         vcs { }
-        schedule {
+        /*schedule {
             schedulingPolicy = weekly {
                 dayOfWeek = ScheduleTrigger.DAY.Monday
                 hour = 12
                 timezone = "Europe/Amsterdam"
             }
             triggerBuild = always()
-        }
+        }*/
     }
     job {
         id = "GradleTests"
